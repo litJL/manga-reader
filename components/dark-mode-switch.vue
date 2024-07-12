ba43b4a6287b1d4c3cee8ae4b82c95c46f9bc4e6
Mike Lenz <private.ml.jl@gmail.com>
2024-07-12 21:06:19 +0200

    feat: dark mode switcher

---
+++ components/dark-mode-switch.vue
@@ -0,0 +1,22 @@
+<template>
+    <ClientOnly>
+        <UToggle
+            v-model="isDark"
+            on-icon="i-heroicons-moon-solid"
+            off-icon="i-heroicons-sun-solid"
+        />
+    </ClientOnly>
+</template>
+
+<script lang="ts" setup>
+const colorMode = useColorMode();
+const isDark = computed({
+    get() {
+        return colorMode.value === "dark";
+    },
+    set(value: boolean) {
+        colorMode.value = value ? "dark" : "light";
+        colorMode.preference = colorMode.value;
+    },
+});
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <h1 class="text-3xl font-bold text-center mt-8">Nuxt 3 Starter</h1>
+  <DarkModeSwitch />
 </template>
 
 <script lang="ts" setup>
