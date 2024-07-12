b7823b9b29ade0911f102e3e058daac745877879
Mike Lenz <private.ml.jl@gmail.com>
2024-07-12 21:19:38 +0200

    feat: created index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,39 @@
 <template>
-  <h1 class="text-3xl font-bold text-center mt-8">Nuxt 3 Starter</h1>
-  <DarkModeSwitch />
+    <UContainer class="flex flex-col gap-8">
+        <h1 class="mt-8 text-center text-3xl font-bold">Nuxt 3 Starter</h1>
+        <UDivider />
+        <UCard>
+            <template #header>
+                <h2 class="flex items-center gap-2 text-xl font-bold">
+                    <UIcon name="i-heroicons-moon-solid" class="size-6" />
+                    Dark mode works out of the box...
+                </h2>
+            </template>
+            <div class="flex items-center gap-2">
+                Try it out
+                <DarkModeSwitch />
+            </div>
+        </UCard>
+        <UCard>
+            <template #header>
+                <h2 class="flex items-center gap-2 text-xl font-bold">
+                    <UIcon
+                        name="i-heroicons-check-badge-solid"
+                        class="size-6"
+                    />
+                    ...as well as Nuxt UI
+                </h2>
+            </template>
+            <div class="flex items-center gap-2">
+                Any component with the 'U' prefix is part of Nuxt UI. Check out
+                the
+                <UButton to="https://ui.nuxtjs.org" size="lg" target="_blank" trailing variant="outline" icon="i-heroicons-document-solid">
+                    documentation
+                </UButton>
+                for more information.
+            </div>
+        </UCard>
+    </UContainer>
 </template>
 
-<script lang="ts" setup>
-
-</script>
+<script lang="ts" setup></script>
